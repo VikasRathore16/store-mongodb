956e47bba6133082ddef4479a6b235047b21c72f
Vikas Rathore <vikasrathore@cedcoss.com>
2022-04-18 12:12:07 +0530

    search product

--- src/app/controllers/ProductController.php
+++ src/app/controllers/ProductController.php
@@ -19,11 +19,18 @@ class ProductController extends Controller
      */
     public function productListAction()
     {
-        $Products = new Products($this->mongo, 'store', 'products');
 
-        $this->view->products = $Products->find();
-        $this->cache->set($this->request->get('locale'), $this->locale);
-        $this->view->t = $this->cache->get($this->request->get('locale'));
+        $Products = new Products($this->mongo, 'store', 'products');
+        $this->view->t = $this->cache->get('en');
+        if ($this->request->has('query')) {
+            $this->view->products = $Products->find(array('$or' => array(array('product_name' => $this->request->get('query')))));
+        }
+        if (!$this->request->has('query') || $this->request->get('submit') == 'all') {
+            $this->view->products = $Products->find();
+        }
+
+        // $this->cache->set($this->request->get('locale'), $this->locale);
+        // $this->view->t = $this->cache->get($this->request->get('locale'));
         if ($this->request->get('msg')) {
             $this->view->msg = "<h6 class='alert alert-success w-25 container text-center'>Added Successfully</h6>";
         }
